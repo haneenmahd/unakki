c94d8b2a71316cd7ab98f3ea245ec717d5ef40d5
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-02 20:06:51 +0530

    Add support for creating and passing in components to run

--- bin/unakki.js
+++ bin/unakki.js
@@ -4,4 +4,14 @@ const args = process.argv;
 
 const unakki = new Unakki();
 
-unakki.init();
+if (args.includes("create") && args.length == 4) {
+  let componentName = args[3];
+
+  unakki.run(componentName);
+
+  process.exit(0);
+} else if (args.length == 3) {
+    throw new Error("Please pass in the name of the component to create!")
+} else {
+    unakki.init();
+}
