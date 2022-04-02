c1eefe462f98b09b3ca44d14aea3101b6278bb4c
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-02 16:32:41 +0530

    use index file to export modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import Config from "./Config/Config";
 import Component from "./Component/Component";
 
-const config = Config.read(process.cwd());
-
-for (let componentConfig of Config.parse(config)) {
-  new Component(componentConfig).init();
-}
+export { Config, Component };
