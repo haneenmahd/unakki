52a0ee150e94a1023143dbfb4eafd66443c3ce50
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-02 19:46:36 +0530

    Remove relative path

--- tests/unakki/unakki.test.js
+++ tests/unakki/unakki.test.js
@@ -3,7 +3,7 @@ import { Unakki } from "../../src/index";
 it("Create a new component set from a multiple component config", () => {
     expect(new Unakki([
         {
-            rootDir: "./lib-tests",
+            rootDir: "lib-tests",
             files: [
                 {
                     name: "componentTest.js"
