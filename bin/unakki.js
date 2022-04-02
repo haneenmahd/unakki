469ae8b0f9ad24aac48fa7f3bb4eba174167415e
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-02 20:18:30 +0530

    Remove throwing pass in component name error

--- bin/unakki.js
+++ bin/unakki.js
@@ -1,4 +1,5 @@
 import { Unakki } from "../src/index";
+import getAvailableComponents from "../src/utils/getAvailableComponents";
 
 const args = process.argv;
 
@@ -11,7 +12,17 @@ if (args.includes("create") && args.length == 4) {
 
   process.exit(0);
 } else if (args.length == 3) {
-    throw new Error("Please pass in the name of the component to create!")
+  const availableComponents = getAvailableComponents();
+
+  availableComponents.map(component => {
+    console.log(
+      `${component.name}: Creates ${component.files.length} ${component.files.length === 1 ? "component" : "components"} in the ${component.rootDir} folder.`
+    );
+  });
+
+  process.exit(0);
 } else {
-    unakki.init();
+  unakki.init();
+
+  process.exit(0);
 }
