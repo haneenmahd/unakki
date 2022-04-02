92fef751aeff1f04e843e34fce2eab15fc7aa40d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-02 18:44:39 +0530

    component: create paths recursively

--- src/Component/Component.js
+++ src/Component/Component.js
@@ -1,6 +1,7 @@
 import path from "path";
 import fs from "fs";
 import checkFileExist from "../utils/checkFileExist";
+import createDirRecursive from "../utils/createDirRecursive";
 
 class Component {
   /**
@@ -19,6 +20,8 @@ class Component {
     
     for (const file of files) {
       const formedPath = path.resolve(rootDir, file.name);
+
+      createDirRecursive(path.dirname(formedPath)); // create directories to make sure that the components can be created.
 
       if (!checkFileExist(formedPath)) fs.writeFileSync(formedPath, file.defaultValue, "utf-8");
 
