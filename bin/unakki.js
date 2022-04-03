5af144f150652600e18472d8c243ce8808ae39cb
Haneen Mahdin <haneenmahdin@gmail.com>
2022-04-03 13:03:04 +0530

    Accept variables from the CLI

--- bin/unakki.js
+++ bin/unakki.js
@@ -1,9 +1,15 @@
+#!/usr/bin/node
+
 import { Unakki } from "../src/index";
 import getAvailableComponents from "../src/utils/getAvailableComponents";
+import chalk from "chalk";
+
+const { bold: boldText, green: greenText, grey: greyText } = chalk;
 
 const args = process.argv;
 
 const unakki = new Unakki();
+const configArgs = args.slice(4);
 
 if (args.includes("create") && args.length == 4) {
   let componentName = args[3];
@@ -16,7 +22,11 @@ if (args.includes("create") && args.length == 4) {
 
   availableComponents.map(component => {
     console.log(
-      `${component.name}: Creates ${component.files.length} ${component.files.length === 1 ? "component" : "components"} in the ${component.rootDir} folder.`
+      `${boldText(component.name)}: Creates ${greenText(
+        component.files.length
+      )} ${
+        component.files.length === 1 ? "component" : "components"
+      } in the ${greyText(component.rootDir)} folder.`
     );
   });
 
